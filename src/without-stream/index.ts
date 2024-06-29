f9c49d9943eb3eca6fc6a7c31b1690619d4e03b6
Varuna <varunavenkatesh@gmail.com>
2024-06-29 09:44:42 +0100

    read the file from the bucket

--- src/without-stream/index.ts
+++ src/without-stream/index.ts
@@ -2,10 +2,10 @@ import { S3 } from "aws-sdk";
 import { APIGatewayProxyHandler } from "aws-lambda";
 
 const s3 = new S3();
-const bucketName = "your-bucket-name";
-const key = "your-file-key";
 
 export const handler: APIGatewayProxyHandler = async (event) => {
+  var bucketName = process.env.BUCKET_NAME || "";
+  var key = event.pathParameters?.key || "two-mb.txt";
   const data = await s3.getObject({ Bucket: bucketName, Key: key }).promise();
   const fileContents = data.Body?.toString("utf-8") || "";
 
