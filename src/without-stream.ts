8f0c3f25244db3778eb93207a0c5bafa23a33228
Varuna <varunavenkatesh@gmail.com>
2024-06-29 18:43:48 +0100

    Improved reading of query parameters

--- src/without-stream.ts
+++ src/without-stream.ts
@@ -5,15 +5,20 @@ const s3 = new S3();
 
 export const handler: APIGatewayProxyHandler = async (event) => {
   var bucketName = process.env.BUCKET_NAME || "";
-  var key = event.pathParameters?.key || "two-mb.txt";
-  const data = await s3.getObject({ Bucket: bucketName, Key: key }).promise();
-  const fileContents = data.Body?.toString("utf-8") || "";
+  console.log(event);
+  var fileName = event.queryStringParameters?.file || "two-mb";
 
+  fileName = fileName + ".txt";
+  const data = await s3
+    .getObject({ Bucket: bucketName, Key: fileName })
+    .promise();
+  const fileContents = data.Body?.toString("utf-8") || "";
+  const result = fileName + " " + fileContents;
   return {
     statusCode: 200,
     headers: {
       "Content-Type": "text/plain",
     },
-    body: fileContents,
+    body: result,
   };
 };
