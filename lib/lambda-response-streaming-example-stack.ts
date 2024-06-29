8ecd4bcee9804b7a7b3ac4cc71e0b76a286760a1
Varuna <varunavenkatesh@gmail.com>
2024-06-29 10:32:18 +0100

    Add api gateway

--- lib/lambda-response-streaming-example-stack.ts
+++ lib/lambda-response-streaming-example-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { aws_lambda, aws_lambda_nodejs } from "aws-cdk-lib";
 import { StringParameter } from "aws-cdk-lib/aws-ssm";
+import * as apigw from "aws-cdk-lib/aws-apigateway";
 
 export class LambdaResponseStreamingExampleStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -23,6 +24,15 @@ export class LambdaResponseStreamingExampleStack extends cdk.Stack {
         runtime: aws_lambda.Runtime.NODEJS_20_X,
         environment: { BUCKET_NAME: bucketName.stringValue },
       }
+    );
+
+    const api = new apigw.RestApi(this, "LambdaResponseStreamingExampleApi", {
+      description: "Lambda Response Streaming Example API",
+    });
+    const withoutStreamPath = api.root.addResource("without-stream");
+    withoutStreamPath.addMethod(
+      "GET",
+      new apigw.LambdaIntegration(functionWithoutStream)
     );
   }
 }
