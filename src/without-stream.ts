0602cd96ea78d0602b0a37e8d7acfdf5133eeca2
Varuna <varunavenkatesh@gmail.com>
2024-06-30 09:59:15 +0100

    refactoring

--- src/without-stream.ts
+++ src/without-stream.ts
@@ -1,14 +1,12 @@
 import { S3 } from "aws-sdk";
 import { APIGatewayProxyHandler } from "aws-lambda";
 
-const s3 = new S3();
-
 export const handler: APIGatewayProxyHandler = async (event) => {
+  const s3 = new S3();
   var bucketName = process.env.BUCKET_NAME || "";
-  console.log(event);
   var fileName = event.queryStringParameters?.file || "two-mb";
-
   fileName = fileName + ".txt";
+
   const data = await s3
     .getObject({ Bucket: bucketName, Key: fileName })
     .promise();
