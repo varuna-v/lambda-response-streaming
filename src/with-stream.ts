2f0f0770120982d77b9b06b5e630d9f46b838b76
Varuna <varunavenkatesh@gmail.com>
2024-06-30 09:59:01 +0100

    refactoring

--- src/with-stream.ts
+++ src/with-stream.ts
@@ -10,16 +10,16 @@ async function myHandler(
   responseStream: ResponseStream
 ): Promise<void> {
   const s3 = new S3();
-
   var bucketName = process.env.BUCKET_NAME || "";
-  console.log(event);
   var fileName = event.queryStringParameters?.file || "two-mb";
 
   fileName = fileName + ".txt";
+
   const data = await s3
     .getObject({ Bucket: bucketName, Key: fileName })
     .promise();
   const fileContents = data.Body?.toString("utf-8") || "";
+
   const result = fileName + " " + fileContents;
   responseStream.setContentType("text/plain");
   responseStream.write(result);
